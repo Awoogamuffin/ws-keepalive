472a24378844f001d9cca9e16fb1d279323cd220
Michael <mbeeson@mac.com>
2020-04-14 19:51:07 +0200

    trying to export classes

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+export { WskClient } from "./WskClient";
+export { WskServer } from "./wskServer";
+
 export function logMessage(msg?: string) {
     return 'LOGGING MESSAGE FROM PACKAGE! ' + (msg || '');
 }
