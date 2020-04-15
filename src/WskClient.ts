ac4d4a4b2b574da1340237ec1bf6773aea5a3679
Michael <mbeeson@mac.com>
2020-04-15 19:35:42 +0200

    so many problems

--- src/WskClient.ts
+++ src/WskClient.ts
@@ -1,16 +1,18 @@
+
 import * as Websocket from 'ws';
 import * as jsonrpc from 'jsonrpc-lite';
 
-export class WskClient extends WebSocket {
+export class WskClient extends Websocket {
 
     uid!: string;
     isAlive: boolean = false;
-
+    
     constructor(url: string, protocols?: string | string[]) {
         super(url, protocols);
         
-        this.onmessage = (e: MessageEvent) => {
-            const parsedMessage: any = jsonrpc.parse(JSON.parse(e.data));
+        this.onmessage = (e: Websocket.MessageEvent) => {
+            console.log('DATA!', e.data);
+            /* const parsedMessage: any = jsonrpc.parse(JSON.parse(e.data));
             console.log('payload!', parsedMessage);
 
             const payload: any = parsedMessage.payload;
@@ -21,7 +23,7 @@ export class WskClient extends WebSocket {
                         this.assignID(payload);
                         break;
                 }
-            }
+            } */
         }
     }
 
